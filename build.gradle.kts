99f1399ba6a11031d6548dab9e4076f1653ee8b7
Iterator <rob.bog@list.ru>
2020-06-15 23:30:48 +0300

    [ANY] Это был самый сложный хелловорлд в моей жизни. Ебанный фистинг. Нахуй нам этот котлин, рома?

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,17 +12,26 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    testCompile("junit", "junit", "4.12")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
+
+val fatJar = task("fatJar", type = Jar::class) {
+    manifest {
+        attributes["Main-Class"] = "ru.alfomine.afmse.AFMSpaceEditor"
+    }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    with(tasks.jar.get() as CopySpec)
+}
+
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+
+    build {
+        dependsOn(fatJar)
     }
 }
---
+++ src/main/kotlin/ru/alfomine/afmse/AFMSpaceEditor.kt
@@ -0,0 +1,10 @@
+package ru.alfomine.afmse
+
+class AFMSpaceEditor {
+    companion object Main {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello World!")
+        }
+    }
+}
